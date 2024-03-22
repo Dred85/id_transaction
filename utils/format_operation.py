107162cdb2a3f777e720b4229725d4717420d110
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-22 16:59:50 +0300

    add commits

--- utils/format_operation.py
+++ utils/format_operation.py
@@ -1,8 +1,16 @@
 import datetime
+
+
 def format_operation(date, description, source, destination, amount, currency):
     """
-    Formats operation string
+    Фоматирует данные из json файла по шаблону:
+- Дата перевода представлена в формате ДД.ММ.ГГГГ (пример: 14.10.2018).
+- Номер карты замаскирован и не отображается целиком в формате  XXXX XX** **** XXXX (видны первые 6 цифр и последние 4,
+разбито по блокам по 4 цифры, разделенных пробелом).
+- Номер счета замаскирован и не отображается целиком в формате  **XXXX
+(видны только последние 4 цифры номера счета).
     """
+    # форматирование даты совершения операции
     formatted_date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f").strftime('%d.%m.%Y')
 
     # форматирование источника денег (from)
@@ -23,8 +31,9 @@ def format_operation(date, description, source, destination, amount, currency):
         masked_destination = ' '.join(destination.split()[:-1]) + ' ' + destination.split()[-1][:4] + ' ' + \
                              destination.split()[-1][4:6] + '**' + ' ' + '****' + ' ' + destination.split()[-1][-4:]
 
+    # форматирование количества и наименования валюты
     formatted_amount = f'{amount} {currency["name"]}'
-
+    # собираем все отформатированные данные в один вывод и возвращаем его из функции
     operation_string = f'{formatted_date} {description}\n{masked_source} -> {masked_destination}\n{formatted_amount}'
 
     return operation_string
