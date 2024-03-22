9c24e1fce2f568ac6d335a45c27bf21c5d23167b
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-22 23:19:04 +0300

    add test_format_operation.py

--- tests/test_format_operation.py
+++ tests/test_format_operation.py
@@ -20,28 +20,29 @@ def test_format_operation():
     """
     Тестирование функции format_operation: Перевод с карты на карту
     """
-    date = "2019-12-03T10:30:00.000"
+    date = "201-12-03T10:30:00.000"
     description = "Перевод с карты на карту"
     source = "MasterCard 1796816785869527"
     destination = "Visa Classic 7699855375169288"
     amount = 31957.58
     currency = {"name": "руб."}
 
-    expected_output = '03.12.2019 Перевод с карты на карту\nMasterCard 1796 81** **** 9527 -> Visa Classic 7699 85** **** 9288\n31957.58 руб.'
+    expected_output = '03.12.2018 Перевод с карты на карту\nMasterCard 1796 81** **** 9527 -> Visa Classic 7699 85** **** 9288\n31957.58 руб.'
 
     assert format_operation(date, description, source, destination, amount, currency) == expected_output
 
 def test_format_operation():
     """
-    Тестирование функции format_operation: Перевод с карты на карту
+    Тестирование функции format_operation: открытие нового счета
     """
     date = "2019-12-03T10:30:00.000"
-    description = "Перевод с карты на карту"
-    source = "MasterCard 1796816785869527"
-    destination = "Visa Classic 7699855375169288"
+    description = "Открытие вклада"
+    source = ""
+    destination = "Счет 19708645243227258125"
     amount = 31957.58
-    currency = {"name": "руб."}
+    currency = {"name": "USD"}
 
-    expected_output = '03.12.2019 Перевод с карты на карту\nMasterCard 1796 81** **** 9527 -> Visa Classic 7699 85** **** 9288\n31957.58 руб.'
+    expected_output = '03.12.2019 Открытие вклада\nОткрытие вклада -> Счет **8125\n31957.58 USD'
 
     assert format_operation(date, description, source, destination, amount, currency) == expected_output
+
