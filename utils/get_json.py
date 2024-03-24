7d0fa0b762deecc551533d524667b45576cecde0
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-24 11:35:32 +0300

    add commit get_json.py

--- utils/get_json.py
+++ utils/get_json.py
@@ -3,12 +3,7 @@ import json
 
 def get_json():
     """
-    Получем дынные из json файла
+    Получаем несортированные дынные из json файла
     """
     with open('C:\\Users\\DRED\\PycharmProject\\id_transactions\\data\\operations.json', 'r', encoding="utf-8") as file:
         return json.load(file)
-
-
-
-
-
