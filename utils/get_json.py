5e9f2375714107c28f6b65746e31810ae2f7be1e
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-25 20:29:35 +0300

    Добавил путь до файла operations.json через абсолютный путь ROOT_DIR

--- utils/get_json.py
+++ utils/get_json.py
@@ -1,16 +1,10 @@
-import os
 import json
 
-from config import ROOT_DIR
 
-
-def get_json():
+def get_json(path_to_file):
     """
     Получаем несортированные дынные из json файла
     """
 
-    # Абсолютный путь до папки utils через ROOT_DIR
-    file_ = os.path.join(ROOT_DIR, 'data', 'operations.json')
-
-    with open(file_, 'r', encoding="utf-8") as file:
+    with open(path_to_file, 'r', encoding="utf-8") as file:
         return json.load(file)
--- utils/main.py
+++ utils/main.py
@@ -1,3 +1,6 @@
+import os
+
+from config import ROOT_DIR
 from utils.sorted_json import sorted_json
 from utils.get_json import get_json
 from utils.format_operation import format_operation
@@ -8,11 +11,14 @@ def main():
 
     # В цикле, при помощи среза sorted_json(get_json())[:5], выводим 5 ближайших к нашай дате транзакции,
     # разделенных пробелами
-    for operation in sorted_json(get_json())[:5]:
+    for operation in sorted_json(get_json(path_to_file))[:5]:
         print(format_operation(operation['date'], operation['description'], operation.get('from', ''),
                                operation.get('to', ''), operation['operationAmount']['amount'],
                                operation['operationAmount']['currency']))
         print()
 
+
+# Путь до файла operations.json через абсолютный путь ROOT_DIR
+path_to_file = os.path.join(ROOT_DIR, 'data', 'operations.json')
 
 main()
