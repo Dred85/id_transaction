e4fb4a54632084f6a8f3b17bd95cd43cc9ba2ad5
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-25 21:13:36 +0300

    add commit

--- utils/sorted_json.py
+++ utils/sorted_json.py
@@ -3,10 +3,11 @@ import datetime
 
 def sorted_json(operations):
     """
-    Сортируем все данные из файла json по дате транзакции ближайшей к нашей дате
+    Фильтруем и сортируем все данные из файла json по дате транзакции ближайшей к нашей дате
     """
-
+    # Фильтрую по статусу выполнения операции 'EXECUTED'
     exectuted_operations = filter(lambda x: x.get("state") == "EXECUTED", operations)
+    # Сортирую по дате транзакции ближайшей к нашей дате уже отфильтрованные данные
     sorted_operations = sorted(exectuted_operations,
                                key=lambda x: datetime.datetime.strptime(x.get('date', '1900-08-26T10:50:58.294041'),
                                                                         "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
