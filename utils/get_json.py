5de18a4c0a808573fa19ad433bab6a1c7a3cdce5
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-25 20:06:07 +0300

    add абсолютный путь current_dir = os.path.dirname(__file__)

--- utils/get_json.py
+++ utils/get_json.py
@@ -6,6 +6,11 @@ def get_json():
     """
     Получаем несортированные дынные из json файла
     """
-    file_ = os.path.join('..', 'data', 'operations.json')
+
+    # Абсолютный путь до папки utils
+    current_dir = os.path.dirname(__file__)
+
+    file_ = os.path.join(current_dir, '..', 'data', 'operations.json')
+
     with open(file_, 'r', encoding="utf-8") as file:
         return json.load(file)
