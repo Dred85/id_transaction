d24ee19308796105f5524f94ae95f7edc81b9583
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-25 20:13:01 +0300

    add абсолютный путь до папки utils через ROOT_DIR

--- utils/get_json.py
+++ utils/get_json.py
@@ -1,16 +1,16 @@
 import os
 import json
 
+from config import ROOT_DIR
+
 
 def get_json():
     """
     Получаем несортированные дынные из json файла
     """
 
-    # Абсолютный путь до папки utils
-    current_dir = os.path.dirname(__file__)
-
-    file_ = os.path.join(current_dir, '..', 'data', 'operations.json')
+    # Абсолютный путь до папки utils через ROOT_DIR
+    file_ = os.path.join(ROOT_DIR, 'data', 'operations.json')
 
     with open(file_, 'r', encoding="utf-8") as file:
         return json.load(file)
