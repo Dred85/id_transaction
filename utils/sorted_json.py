c78e04ba0bd3437ebdcef38ca42a88b016abdef4
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-25 21:02:52 +0300

    Добавил фильтрацию exectuted_operations

--- utils/sorted_json.py
+++ utils/sorted_json.py
@@ -6,7 +6,8 @@ def sorted_json(operations):
     Сортируем все данные из файла json по дате транзакции ближайшей к нашей дате
     """
 
-    sorted_operations = sorted(operations,
+    exectuted_operations = filter(lambda x: x.get("state") == "EXECUTED", operations)
+    sorted_operations = sorted(exectuted_operations,
                                key=lambda x: datetime.datetime.strptime(x.get('date', '1900-08-26T10:50:58.294041'),
                                                                         "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
     return sorted_operations
