a2cf7044eeb4ee039ab0db74ca7c4f446ba873ed
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-26 18:32:10 +0300

    add commit

--- main.py
+++ main.py
@@ -10,33 +10,23 @@ from utils.format_operation import format_operation
 def main():
     """" Окончательный Вывод 5 ближайших к нашай дате транзакции """
 
-    # В цикле, при помощи среза sorted_json(get_json())[:5], выводим 5 ближайших к нашай дате транзакции,
-    # разделенных пробелами
-    # for operation in sorted_json(get_json(path_to_file))[:5]:
-    #     print(format_operation(operation['date'], operation['description'], operation.get('from', ''),
-    #                            operation.get('to', ''), operation['operationAmount']['amount'],
-    #                            operation['operationAmount']['currency']))
-    #     print()
+
+# Путь до файла operations.json через абсолютный путь ROOT_DIR
 path_to_file = os.path.join(ROOT_DIR, 'data', 'operations.json')
 
-all_operations = get_json(path_to_file) # все транзакции без фильтрации и сортировки
-
-executed_operations = get_executed_only(all_operations) # фильтруем по EXECUTED
-
-sorted_operations = sorted_json(executed_operations) # сортируем
-
-five_for_formating = sorted_operations[:5] # берем пять
-for operation in five_for_formating: # цикл для формирования и вывода
+# все транзакции без фильтрации и сортировки
+all_operations = get_json(path_to_file)
+# фильтруем по EXECUTED
+executed_operations = get_executed_only(all_operations)
+# сортируем по дате операции ближайшей к нам
+sorted_operations = sorted_json(executed_operations)
+# В цикле выводим первые пять операций и формируем вывод как по ТЗ
+five_for_formating = sorted_operations[:5]  # берем пять
+for operation in five_for_formating:  # цикл для формирования и вывода
     print(format_operation(operation['date'], operation['description'], operation.get('from', ''),
                            operation.get('to', ''), operation['operationAmount']['amount'],
                            operation['operationAmount']['currency']))
     print()
-
-
-
-
-# Путь до файла operations.json через абсолютный путь ROOT_DIR
-
 
 if __name__ == '__main__':
     main()
--- utils/sorted_json.py
+++ utils/sorted_json.py
@@ -7,7 +7,7 @@ def sorted_json(operations):
     """
 
     # Сортирую по дате транзакции ближайшей к нашей дате уже отфильтрованные данные
-    sorted_operations = sorted(exectuted_operations,
+    sorted_operations = sorted(operations,
                                key=lambda x: datetime.datetime.strptime(x.get('date', '1900-08-26T10:50:58.294041'),
                                                                         "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
     return sorted_operations
