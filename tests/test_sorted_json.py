9511dc3612df38a5fb451dcd63a22aacfcc1ef62
Olonov AleKsandr <08111985adrolv@gmail.com>
2024-03-30 22:53:36 +0300

    fix commit

--- tests/test_sorted_json.py
+++ tests/test_sorted_json.py
@@ -6,6 +6,8 @@ from utils.get_json import get_json
 import pytest
 
 path_to_file = os.path.join(ROOT_DIR, 'data', 'operations.json')
+
+
 # Создаем фикстуру, которая запускается перед каждым тестом
 @pytest.fixture
 def coll():  # имя фикстуры
@@ -17,8 +19,6 @@ expected_output_sort_1 = 114832369
 expected_output_sort_2 = 560813069
 expected_output_sort_3 = 154927927
 expected_output_sort_4 = 482520625
-
-
 
 
 def test_sorted_json(coll):
--- utils/get_executed_only.py
+++ utils/get_executed_only.py
@@ -1,5 +1,8 @@
+
 def get_executed_only(operations):
     """Фильтрую по статусу выполнения операции 'EXECUTED'"""
 
     exectuted_operations = filter(lambda x: x.get("state") == "EXECUTED", operations)
     return exectuted_operations
+
+
