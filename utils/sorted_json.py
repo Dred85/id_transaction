3e45f736ff3741e54eb631953a79e2e3db0d81b2
Olonov AleKsandr <08111985adrolv@gmail.com>
2024-03-31 20:56:50 +0300

    Python 3.12.2

--- utils/sorted_json.py
+++ utils/sorted_json.py
@@ -2,12 +2,11 @@ def sorted_json(operations):
     """
     Cортируем все данные из файла json по дате транзакции ближайшей к нашей дате
     """
-
     # Сортирую по дате транзакции ближайшей к нашей дате уже отфильтрованные данные
     sorted_operations = sorted(operations, key=lambda x: x.get('date', '0'), reverse=True)
     return sorted_operations
 
-#
+
 if __name__ == '__main__':
     operations = [{"date": "030"}, {"date": "005"}, {"date": "200"}]
     print(sorted_json(operations))
